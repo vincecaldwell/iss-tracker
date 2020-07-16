97c29fb45f09dd8979fc5ea0cda3a80561e9f20b
vincent caldwell <vcaldwel8@yahoo.com>
2020-07-16 18:24:30 -0400

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-
-This is a web-app that provides information about the International Space Station. 
-
-Built w/ React.js
-
-***Build still in progress***
---
+++ precache-manifest.baf8938b21e081199030e302b18d339e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6740bdc3f55f574f3d35ea1cc2257361",
+    "url": "/iss-tracker/index.html"
+  },
+  {
+    "revision": "7f22a99b91b344ae7a34",
+    "url": "/iss-tracker/static/css/main.7324aa11.chunk.css"
+  },
+  {
+    "revision": "8d2a3ed5822adeb14cea",
+    "url": "/iss-tracker/static/js/2.3eb20973.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iss-tracker/static/js/2.3eb20973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f22a99b91b344ae7a34",
+    "url": "/iss-tracker/static/js/main.a195c377.chunk.js"
+  },
+  {
+    "revision": "6e1c98031d778f026e1f",
+    "url": "/iss-tracker/static/js/runtime-main.bce6a6b1.js"
+  }
+]);
