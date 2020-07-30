27c0c24320108a394fe01052163de41e047bea4b
vincent caldwell <vcaldwel8@yahoo.com>
2020-07-30 18:01:02 -0400

    Updates

---
+++ precache-manifest.3e72fac188d5e02da13a8b7bd45e6668.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9504e0efe5dac5c572a08637653535e",
+    "url": "/iss-tracker/index.html"
+  },
+  {
+    "revision": "ef8c91a1a5078c521b81",
+    "url": "/iss-tracker/static/css/main.28521cf1.chunk.css"
+  },
+  {
+    "revision": "12f2a645321252126d3a",
+    "url": "/iss-tracker/static/js/2.294a9d46.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/iss-tracker/static/js/2.294a9d46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef8c91a1a5078c521b81",
+    "url": "/iss-tracker/static/js/main.89583138.chunk.js"
+  },
+  {
+    "revision": "6e1c98031d778f026e1f",
+    "url": "/iss-tracker/static/js/runtime-main.bce6a6b1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/iss-tracker/precache-manifest.baf8938b21e081199030e302b18d339e.js"
+  "/iss-tracker/precache-manifest.3e72fac188d5e02da13a8b7bd45e6668.js"
 );
 
 self.addEventListener('message', (event) => {
