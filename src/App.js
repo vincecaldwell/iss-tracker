4a81cb648868bc45bf0d1b5fab9b49eb219bd4fd
vincent caldwell <vcaldwel8@yahoo.com>
2020-08-07 19:41:49 -0400

    added isLoading and error state, set up error handling on api call and render function

--- src/App.js
+++ src/App.js
@@ -5,39 +5,65 @@ import { Footer } from './components/footer/footer.component.jsx';
 import { DataCard } from './components/data-card/data-card.component.jsx';
 import { Button } from 'react-bootstrap';
 
-class App extends Component {
+const API = 'https://api.wheretheiss.at/v1/satellites/25544?units=miles';
 
+
+class App extends Component {
   constructor() {
     super();
   this.state ={
-      stationData: []
+      stationData: [],
+      isLoading: false,
+      error: null
   };
 }
 
+//SPACE STATION DATA FETCH
 componentDidMount(){
-  //SPACE STATION DATA FETCH
-  fetch('https://api.wheretheiss.at/v1/satellites/25544?units=miles')
-  .then(response => response.json())
-  .then(data => this.setState({stationData: data}));
+  this.setState({ isLoading: true });
+  fetch(API)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Something went wrong ...');
+      }
+    })
+    .then(data => this.setState({stationData: data, isLoading: false}))
+    .catch(error => this.setState({ error, isLoading: false }));
 }
   
 
   
 
   //Refresh Data when button clicked
- handleClick = () => {
-   fetch('https://api.wheretheiss.at/v1/satellites/25544?units=miles')
-  .then(response => response.json())
-  .then(data => this.setState({stationData: data}));
- }
+ 
+  handleClick = () => {
+
+  fetch(API)
+    .then(response => response.json())
+    .then(data => this.setState({stationData: data}));
+  }
+
+
 
 
 
 render() {
+  const { stationData, isLoading, error } = this.state;
+
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return(
         <div className= "container">
             <Header />
-            <DataCard station = {this.state.stationData}/>
+            <DataCard station = {stationData}/>
             <Button
               type="button"
               className="refresh-button" 
